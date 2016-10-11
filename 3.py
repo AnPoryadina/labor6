f1f4c69b8fa260454af0c4ba8ab8fdedc3dcae16
AnPoryadina <an.poryadina@mail.ru>
2016-10-11 23:11:29 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,19 +1,17 @@
 __author__ = 'student'
-if __name__=="__main__":
-
-import numpy as np
 import random
+import numpy as np
 import matplotlib.pyplot as plt
 
 def get_percentile(values, bn):
     step = 100/bn
-    result = []
-    act = 0
+    res = []
+    do = 0
     for o in range (bn):
-         result.append(np.percentile(values, act))
-         act += step
-    result[0] = 0.0
-    return result
+         res.append(np.percentile(values, do))
+         do += step
+    res[0] = 0.0
+    return res
 
 def get_percentile_number(value, percentiles):
     i = 0
@@ -25,13 +23,17 @@ def get_percentile_number(value, percentiles):
             break
     return index
 
+
 def value_equalization(value, percentiles, add_random = False):
     idx = get_percentile_number(value, percentiles)
     step = 1/len(percentiles)
-    new_value = idx*step
+    new_one = idx*step
     if add_random == True:
         new_value = idx*step + random.uniform(idx*step, (idx+1)*step)
-    return new_value
+    return new_one
 
 
-
+def values_equalization(values, percentiles, add_random=False):
+    for i in range(len(values)):
+        values[i] = value_equalization(values[i], percentiles, add_random)
+    return values
